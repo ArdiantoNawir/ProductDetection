fcc3db0f7ee32fd555efa3691e22587bf13d4575
yangfebriana <yangfebriana04@gmail.com>
2021-05-18 21:21:23 +0700

    tambah carouselview pada home fragment

--- app/src/main/java/com/capstone/productdetection/ui/home/HomeFragment.kt
+++ app/src/main/java/com/capstone/productdetection/ui/home/HomeFragment.kt
@@ -8,19 +8,23 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.capstone.productdetection.R
 import com.capstone.productdetection.databinding.FragmentHomeBinding
+import com.synnapps.carouselview.ImageListener
 
 class HomeFragment : Fragment() {
 
     private lateinit var homeViewModel: HomeViewModel
     private var _binding: FragmentHomeBinding? = null
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         homeViewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
+
+//carouselView.pageCount = carouselImages.size
+//carouselView.setImageListener(imageListener)
+// aku lihatnya di homeactivity di step nya, tapi kalo dimasukin fragment kurang tau aku
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
@@ -35,5 +39,15 @@ class HomeFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+    val carouselImages = intArrayOf(
+        R.drawable.ikm_1,
+        R.drawable.ikm_2,
+        R.drawable.ikm_3,
+        R.drawable.ikm_4
+    )
+
+    val imageListener = ImageListener {position, imageView ->
+        imageView.setImageResource(carouselImages[position])
     }
 }
